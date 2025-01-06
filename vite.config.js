def39994bb7a76bb266b533986b7676def97725f
Agungfir08 <agung.firmansyah@ui.ac.id>
2025-01-06 22:57:44 +0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
             output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('react-dom')) {
-                            return 'vendor_react_dom';
-                        }
-                        if (id.includes('react-router-dom')) {
-                            return 'vendor_react_router';
-                        }
-                        if (id.includes('aos')) {
-                            return 'vendor_animation';
-                        }
-                        if (id.includes('react-lazy-load-image-component')) {
-                            return 'vendor_image_loading';
-                        }
-                        return 'vendor';
-                    }
+                manualChunks: {
+                    'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+                    animation: ['aos'],
+                    'image-loading': ['react-lazy-load-image-component'],
+                    analog: ['./src/constants/Analog.js'],
+                    projects: ['./src/constants/Projects.js'],
+                    resume: ['./src/constants/Resumes.js'],
                 },
             },
         },
