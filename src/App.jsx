8ddbcaa6486dcde4b60c3d39ea3923c853f3ff20
Agungfir08 <agung.firmansyah@ui.ac.id>
2025-01-14 21:52:11 +0700

    add vercel speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
         <>
             <RouterProvider router={router} />
             <Analytics />
+            <SpeedInsights />
         </>
     );
 }
